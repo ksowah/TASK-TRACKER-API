92db673641579cbb6445f22508f4f3adbd1a8b18
ksowah <sowahkelvin640@gmail.com>
2022-07-23 21:09:16 +0000

    some changes

--- dist/server.js
+++ dist/server.js
@@ -5,10 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const todoRoute_1 = __importDefault(require("./routes/todoRoute"));
+const db_1 = __importDefault(require("./config/db"));
+const errorHandller_1 = require("./middleware/errorHandller");
 dotenv_1.default.config();
+(0, db_1.default)();
 const app = (0, express_1.default)();
-const PORT = process.env.PORT;
-app.get("/", (req, res) => {
-    res.send("<h1>Chale this is Typescript server</h1>");
-});
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.use(express_1.default.json());
+// app.use(express.urlencoded({extended: false}))
+app.use("/api/todo", todoRoute_1.default);
+app.use(errorHandller_1.handleError);
+app.listen(PORT, () => console.log(`⚡Server running on port ${PORT}`));
+/***  TODO: FIX CLIENT ERROR MESSAGE */
