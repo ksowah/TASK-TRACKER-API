a3d4f4cfd2335a1f25cf86b7aaa5850a0c48639c
ksowah <sowahkelvin640@gmail.com>
2022-07-23 19:36:09 +0000

    create controllers for todos

--- controller/todoController.ts
+++ controller/todoController.ts
@@ -1,7 +1,53 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import todoSchema from "../models/todoModel"
 
 
+export const getTodo = async (req: Request, res: Response) => {
+    const todos = await todoSchema.find({})
+    if(!todos){
+        res.status(400)
+        throw new Error("No todos available")
+    }
+    res.status(200).json(todos)
+}
 
-export const getTodo = (req: Request, res: Response) => {
-    res.status(200).send("<h1>This is a typescript express server</h1>")
+export const postTodo = async (req: Request, res: Response) => {
+    if(!req.body.todo){
+        res.status(400)
+        console.log("theres no body");
+        throw new Error("Please add a todo")
+    }
+
+    console.log(req.body)
+
+    const todo = await todoSchema.create({
+        todo: req.body.todo
+    })
+
+    res.status(200).json(todo)
+}
+
+export const deleteTodo = async (req: Request, res: Response) => {
+    const delTodo = await todoSchema.findById(req.params.todoID)
+
+    if(!delTodo){
+        res.status(400)
+        throw new Error("this Todo does not exist")
+    }
+
+    res.send({todo: delTodo})
+    await delTodo?.remove()
+    
+}
+
+export const updateTodo = async (req: Request, res: Response) => {
+    const todo = await todoSchema.findById(req.params.todoID) 
+
+    if(!todo){
+        res.status(400)
+        throw new Error("this Todo does not exist")
+    }
+
+    const updatedTodo = await todoSchema.findByIdAndUpdate(req.params.todoID, req.body, {new: true})
+    res.status(200).send(updatedTodo)
 }
