6751935673b0aac28e3a2f81b7615a0b6533deac
ksowah <sowahkelvin640@gmail.com>
2022-07-23 19:35:33 +0000

    build server

--- server.ts
+++ server.ts
@@ -1,13 +1,21 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express } from 'express';
 import dotenv from 'dotenv';
 import router from "./routes/todoRoute"
+import connectDB from './config/db';
+import { handleError } from './middleware/errorHandller';
 
 dotenv.config();
 
+connectDB()
+
 const app: Express = express()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 
+app.use(express.json())
+// app.use(express.urlencoded({extended: false}))
 app.use("/api/todo", router)
+app.use(handleError)
 
+app.listen(PORT, ()=> console.log(`⚡Server running on port ${PORT}`))
 
-app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
+/***  TODO: FIX CLIENT ERROR MESSAGE */
